5774c24d286ad87d4bfa64af12d27a40cf483ecc
Jianshen He <jianshenhe@Jianshens-MacBook-Pro.local>
2014-10-29 19:26:05 -0500

    Initial Commit

---
+++ hw3AI/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw3AI
+//
+//  Created by Jianshen He on 10/29/14.
+//  Copyright (c) 2014 Jianshen He. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
